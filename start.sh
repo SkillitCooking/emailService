e1f582b9fce9a22b1b15341b9eb75bc77948a772
Dane Louis Bratz <danebratz@gmail.com>
2017-10-25 11:19:36 -0700

    shell script

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# gets npm path, goes into emailService's home dir
+# uses stored npm to run the email node script
+NPM=$(which npm)
+cd /home/dane/emailService
+$NPM run start >> /home/dane/emailService/logs/cron.log
